ad6a85f9defc8c957cb59f11cc71435fd10020d2
loukkyy <servan.fichet@gmail.com>
2021-05-03 00:10:14 +0200

    add auto-generated swagger.json

---
+++ api-definition.js
@@ -0,0 +1,23 @@
+module.exports = {
+    openapi: '3.0.0',
+    info: {
+      title: 'Role Based Access Control API',
+      version: '1.0.0',
+      description:
+        'This is a REST API application made with Express. It retrieves data from JSONPlaceholder.',
+      license: {
+        name: 'Licensed Under MIT',
+        url: 'https://spdx.org/licenses/MIT.html',
+      },
+      contact: {
+        name: 'JSONPlaceholder',
+        url: 'https://jsonplaceholder.typicode.com',
+      },
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+        description: 'Development server',
+      },
+    ],
+  };
